bde7cbd708e1bd7cac1e310985defa17dfd997a4
andersoongamerx-sudo <andersoongamerx@gmail.com>
2026-09-14 15:43:39 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE='motogasto-pwa-v1';
+const ASSETS=[
+  '/Motogasto/',
+  '/Motogasto/index.html',
+  '/Motogasto/styles.css',
+  '/Motogasto/app.js',
+  '/Motogasto/manifest.json',
+  '/Motogasto/icon-192.png',
+  '/Motogasto/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request).then(r => r || caches.match('/Motogasto/')))
+  );
+});
